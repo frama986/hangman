5ff6697253d6860696eef371f08b354283e2a9a6
Francesco <francesco.malatto@gmail.com>
2016-05-26 18:09:57 +0200

    added log function

--- hangmanWebApp/src/com/frama/controller/MainController.java
+++ hangmanWebApp/src/com/frama/controller/MainController.java
@@ -3,6 +3,8 @@ package com.frama.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,9 +15,18 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 public class MainController {
    
+   private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+   
    private static final String VIEW_INDEX = "index";
    private static final String VIEW_LOGIN = "login";
-
+   
+   /**
+    * Main function.
+    * Dispatches the first request and checks if user is logged in. 
+    * @param model spring model
+    * @param request http request
+    * @return
+    */
    @RequestMapping(value="/", method=RequestMethod.GET)
    public String welcome(ModelMap model, HttpServletRequest request) {
       
@@ -23,15 +34,24 @@ public class MainController {
       String sessionUser = (String) session.getAttribute("username");
       
       if(sessionUser == null) {
+         logger.debug("[welcome] new user found: redirect to login.");
          return VIEW_LOGIN;
       }
+      
+      logger.debug("[welcome] Username : {}", sessionUser);
       
       model.addAttribute("username", sessionUser);
 
       return VIEW_INDEX;
    }
 
-
+   /**
+    * Login function.
+    * Adds the user to session object and redirect to welcome main function.
+    * @param username name of user used for the authentication
+    * @param request http request
+    * @return
+    */
    @RequestMapping(value="/login", method=RequestMethod.POST)
    public RedirectView login(@ModelAttribute("username") String username, HttpServletRequest request) {
 
@@ -39,6 +59,7 @@ public class MainController {
 
       if(username != null && !username.isEmpty()) {
          session.setAttribute("username", username);
+         logger.debug("[login] New user : {}", username);
       }
 
       RedirectView redirectView = new RedirectView();
