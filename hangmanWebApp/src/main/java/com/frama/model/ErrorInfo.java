39621626251823813dd8127b48c07bd03a09419e
Francesco <francesco.malatto@gmail.com>
2016-06-03 00:35:28 +0200

    added cookies management

---
+++ hangmanWebApp/src/main/java/com/frama/model/ErrorInfo.java
@@ -0,0 +1,43 @@
+package com.frama.model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ErrorInfo {
+
+   private String errCode;
+   
+   private String errMsg;
+   
+   private String errTime;
+
+   public String getErrCode() {
+      return errCode;
+   }
+
+   public void setErrCode(String errCode) {
+      this.errCode = errCode;
+   }
+
+   public String getErrMsg() {
+      return errMsg;
+   }
+
+   public void setErrMsg(String errMsg) {
+      this.errMsg = errMsg;
+   }
+
+   public String getErrTime() {
+      return errTime;
+   }
+
+   public void setErrTime(String errTime) {
+      this.errTime = errTime;
+   }
+
+   public ErrorInfo(String errCode, String errMsg) {
+      this.errCode = errCode;
+      this.errMsg = errMsg;
+      this.errTime = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").format(new Date());
+   }
+}
